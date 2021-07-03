f60cb65e01ce8ec642430a1f89d6f8abcb24e717
Ismet Gjikolli <gjikolliismet@gmail.com>
2021-07-03 16:18:59 -0400

    Homework 1

---
+++ src/Summer2021/Homework1.java
@@ -0,0 +1,101 @@
+package Summer2021;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        /**
+         *
+         * 1. Numbers of hours in a day byte
+         */
+
+        byte numHoursInDay = 24;
+
+        // Print --> In a day there are 24 hours
+
+        System.out.println("In a day there are 24 hours : " + numHoursInDay);
+
+        /**
+         *
+         * 2. Max no. of days in a year
+         */
+
+        int numDaysInYear = 366;
+
+        // Print --> Maximal number of days in a year is
+
+        System.out.println("\nMaximal number of days in a year is : " + numDaysInYear);
+
+        /**
+         *
+         * 3. Total number of employees in Amazon
+         */
+
+        int numOfEmpInAmazon = 1298000;
+
+        // Print --> Total number of employees in Amazon is
+
+        System.out.println("\nTotal number of employees in Amazon is : " + numOfEmpInAmazon);
+
+        /**
+         *
+         * 4. Population in Kosovo
+         */
+
+        int popInKosovo = 1873000;
+
+        // Print --> The population in Kosovo is
+
+        System.out.println("\nThe population in Kosovo is : " + popInKosovo);
+
+        /**
+         *
+         * 5. Average mortgage rate in US
+         */
+
+        float morRateInUS = 4.14f;
+
+        // Print --> The average mortgage rate in US is
+
+        System.out.println("\nThe average mortgage rate in US is : " + morRateInUS);
+
+        /**
+         *
+         * 6. Balance in a bank account
+         */
+
+        double accountBalance = 534868.56;
+
+        // Print --> My bank account balance is
+
+        System.out.println("\nMy bank account balance is : " + accountBalance);
+
+        /**
+         *
+         * 7. Does the sun rise from the west?
+         */
+
+        boolean doesSunRiseInWest = false;
+
+        // Print --> Does the Sun rise from the West?
+
+        System.out.println("\nDoes the sun rise from the West? " + doesSunRiseInWest);
+
+        /**
+         *
+         * 8. Initials of my name
+         */
+
+        char initials = 'i';
+
+        // Print --> Initials of my name are
+
+        System.out.println("\nInitials of my name is : " + initials);
+
+
+
+
+
+    }
+
+}
